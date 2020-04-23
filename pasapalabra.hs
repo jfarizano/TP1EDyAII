d96d32404f9bae96633fad968e7072c561196092
jfarizano <jifarizano@hotmail.com>
2020-04-23 20:30:28 -0300

    cambios, ordernar o no ordenar?

--- pasapalabra.hs
+++ pasapalabra.hs
@@ -46,9 +46,6 @@ delete s@(k:ks) n@(Node c v E E r) | k == c = if ks == [] then r else n
 delete (k:ks) n@(Node c v E m E) | k == c = if ks == [] then Node c Nothing E m E
                                                         else convertirEnHoja (Node c v E (delete ks m) E)
                                  | otherwise = n
-delete s@(k:ks) n@(Node c v l E r) | k == c = if ks == [] then reorder l r else n
-                                   | k < c = Node c v (delete s l) E r
-                                   | otherwise = Node c v l E (delete s r)
 delete s@(k:ks) n@(Node c v l m r) | k == c = if ks == [] then Node c Nothing l m r
                                                           else Node c v l (delete ks m) r
                                    | k < c = Node c v (delete s l) m r
@@ -58,11 +55,6 @@ convertirEnHoja :: Ord k => TTree k v -> TTree k v
 convertirEnHoja (Node c Nothing E E E) = E
 convertirEnHoja (Node c v E E E) = (Leaf c (fromJust v))
 convertirEnHoja n = n
-
-reorder :: Ord k => TTree k v -> TTree k v -> TTree k v
-reorder E r = r
-reorder l E = l
-reorder ln@(Node k v l m r) rn@(Node c b i e d) = undefined
 
 -- Dado un  ́arbol devuelve una lista ordenada con las claves del mismo
 keys :: Ord k => TTree k v -> [[k]]
@@ -89,13 +81,4 @@ instance Ord k => Dic [k] v (TTree k v) where
   eliminar = undefined
   claves = keys
 
-simple = Node 'r' Nothing E (Node 'e' (Just 16) (Node 'a' (Just 20) E E E) E E) E
-
 t = insert "se" 8 (insert "sin" 7 (insert "si" 4 (insert "ras" 1 (insert "res" 4 (insert "red" 9 (insert "reo" 2 (insert "re" 16 E)))))))
-
-b = insert "holes" 4 (insert "hole" 1 (insert "hola" 2 (insert "ho" 3 E)))
-
-q = insert "cosde" 8 (insert "cosd" 7 (insert "cosc" 6 (insert "cosbe" 5 (insert "cosb" 4 (insert "cosi" 3 (insert "cosa" 2 (insert "cose" 1 E)))))))
-a = keys q
--- o = delete "cose" q
--- b = keys o
